3199ff492e045f201f154b100f1a1dc99596c2c7
faber9090 <faber.fernadez@est.iudigital.edu.co>
2022-01-30 15:46:36 -0500

    :construction:implementing restexception usuariodto service impl usuario

---
+++ src/main/java/co/edu/iudigital/app/service/iface/IUsuarioService.java
@@ -0,0 +1,19 @@
+package co.edu.iudigital.app.service.iface;
+
+import java.util.List;
+
+import co.edu.iudigital.app.dto.UsuarioDto;
+import co.edu.iudigital.app.exception.RestException;
+import co.edu.iudigital.app.model.Usuario;
+
+public interface IUsuarioService {
+	
+	public List<UsuarioDto> listUsers () throws RestException;
+	
+	public Usuario listUsers(Long id) throws RestException;
+	
+	public Usuario saveUser(Usuario usuario) throws RestException;
+	
+	public Usuario listByUsername(String username);
+
+}
